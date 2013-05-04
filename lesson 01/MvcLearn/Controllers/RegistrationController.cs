7697ae0d4d4f1a22908245c9d7078cc1c1316c6c
wsugiri <wsugiri@gmail.com>
2013-05-04 23:13:00 +0700

    lesson 01

---
+++ lesson 01/MvcLearn/Controllers/RegistrationController.cs
@@ -0,0 +1,77 @@
+﻿using MvcLearn.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcLearn.Controllers
+{
+    public class RegistrationController : DbController
+    {
+        public ActionResult Index()
+        {
+            var list = ctx.Registrations;
+            return View(list);
+        }
+
+        public ActionResult Create()
+        {
+            var model = new Registration();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Registration model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.RegID = Guid.NewGuid().ToString();
+                ctx.Registrations.Add(model);
+                ctx.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public ActionResult Edit(string id)
+        {
+            var model = ctx.Registrations.Find(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Registration model)
+        {
+            if (ModelState.IsValid)
+            {
+                ctx.Entry(model).State = EntityState.Modified;
+                ctx.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public ActionResult Details(string id)
+        {
+            var model = ctx.Registrations.Find(id);
+            return View(model);
+        }
+
+        public ActionResult Delete(string id)
+        {
+            var model = ctx.Registrations.Find(id);
+            return View(model);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            var model = ctx.Registrations.Find(id);
+            ctx.Registrations.Remove(model);
+            ctx.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ lesson 01/MvcLearn/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcLearn.Models
+{
+    public class DataContext : DbContext
+    {
+        public IDbSet<Registration> Registrations { get; set; }
+    }
+}
