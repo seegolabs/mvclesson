cd2002174efbb04081334337d36c892fa35f1aa9
wsugiri <wsugiri@gmail.com>
2013-05-05 10:33:18 +0700

    initial

---
+++ lesson 02/MvcLearn/Controllers/DbController.cs
@@ -0,0 +1,14 @@
+﻿using MvcLearn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcLearn.Controllers
+{
+    public class DbController : Controller
+    {
+        protected DataContext ctx = new DataContext();
+    }
+}
---
+++ lesson 02/MvcLearn/Models/Registration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcLearn.Models
+{
+    [Table("Registration")]
+    public class Registration
+    {
+        [Key]
+        public string RegID { get; set; }
+        public string RegName { get; set; }
+        public DateTime RegDate { get; set; }
+        public string Address { get; set; }
+        public int Status { get; set; }
+        public bool IsApproved { get; set; }
+    }
+}
